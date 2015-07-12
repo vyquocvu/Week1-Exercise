9afae5d5eb1085f4044307da0d1883fecf47f821
vyquocvu <vyquocvu@gmail.com>
2015-07-12 20:52:32 +0700

    exercise

---
+++ exercise1.rb
@@ -0,0 +1,4 @@
+﻿puts"Hello world"
+puts"What's your name?"
+astr = gets.chomp
+puts "Hello #{astr}. Have a nice day!"
---
+++ exercise2.rb
@@ -0,0 +1,33 @@
+﻿class Document
+  def initialize(title,author, content)
+    @title = title
+    @author = author
+    @content = content
+  end
+  
+  def description
+    puts "#{@title} and I was created by #{@author}! #{@content}"
+  end
+  
+  def get_title
+    @title
+  end
+  def get_author
+    @author
+  end
+  def get_content
+    @content
+  end
+  def update_content(new_content)
+    @content = new_content    
+  end
+  
+end
+
+trainee = Document.new('Train', 'Vy Quoc Vu', 'This is content')
+trainee.description
+puts " Title: #{trainee.get_title}"
+puts " Author: #{trainee.get_author}"
+puts " Content: #{trainee.get_content}"
+trainee.update_content("This is new content, not content")
+puts " New Content: #{trainee.get_content}"
